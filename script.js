d43af850434a7b1240ed1ef60c9bd36c6cb03825
Binar97 <88194649+Binar97@users.noreply.github.com>
2026-03-05 22:29:47 +0300

    1st Commit
    Null

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+function openPackage(evt, pkgName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active");
+    }
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(pkgName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
+</script>
